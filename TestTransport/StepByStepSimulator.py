5d6baf6d57723e50315086bcd3d042c49b86a501
li <crischenli@gmail.com>
2015-06-30 17:50:07 +0800

    link queue calculation is correct now

--- TestTransport/StepByStepSimulator.py
+++ TestTransport/StepByStepSimulator.py
@@ -2,6 +2,9 @@ __author__ = 'li'
 
 import random
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 from Src import Flow, Link
 from TUnits import *
@@ -10,15 +13,16 @@ from TUnits import *
 # run simulator step by step, running for 100 milliseconds
 # simulating multiple flows over one link
 PROTO = ['d2tcp', 'mcp', 'tcp', 'dctcp']
+# PROTO = ['d2tcp']
 
 class StepByStepSimulator:
     # setting up environment
     def __init__(self,
-                 RTT=0.000001*80,
+                 RTT=0.000001*100,
                  SIMTIME=0.1,
                  CONCURRENCY=5,
                  FLOWSIZEMEAN=0.5,
-                 FLOWDDLMEAN = 0.001*50,
+                 FLOWDDLMEAN=0.001*50,
                  ECN=64,
                  MAXBUFFER=12,
                  LINKCAP=10,
@@ -26,11 +30,10 @@ class StepByStepSimulator:
         self.RTT = RTT
         self.SIMTIME = SIMTIME
         self.Steps = int(round(SIMTIME / RTT))
-        self.MAXNUMFLOW = CONCURRENCY * self.Steps # This is concurrency of flows
+        self.MAXNUMFLOW = CONCURRENCY * self.Steps  # This is concurrency of flows
         self.FLOWSIZEMEAN = FLOWSIZEMEAN * MB
         self.FLOWDDLMEAN = FLOWDDLMEAN
 
-        # PROTO = ['d2tcp']
         # ECN Threshold
         self.ECN = ECN * PKTSIZE
         self.MAXBUFFER = MAXBUFFER * MB
@@ -42,7 +45,7 @@ class StepByStepSimulator:
 
     def geninput(self):
         # Return a list of flows
-        flowlist=[]
+        flowlist = []
         # flow format
         print 'Generating input'
         for fid in range(self.MAXNUMFLOW):
@@ -63,10 +66,11 @@ class StepByStepSimulator:
         return flowlist
 
     def Rate(self, flow, CongestionPercentage, LinkQueue):
+
         if flow.transport == 'dctcp':
             if CongestionPercentage > 0.002:
                 cWin = flow.bw * self.RTT
-                cWin = cWin * (1 - CongestionPercentage/2.0)
+                cWin *= (1 - CongestionPercentage/2.0)
                 flow.bw = cWin/self.RTT
             else:
                 flow.bw += 1*(PKTSIZE/self.RTT)
@@ -82,7 +86,7 @@ class StepByStepSimulator:
                 cWin = flow.bw * self.RTT
                 Urgency = max(0, min(5.0, flow.remainSize/flow.remainTime))
                 try:
-                    cWin = cWin*(1 - (CongestionPercentage**Urgency)/2.0)
+                    cWin *= (1 - (CongestionPercentage**Urgency)/2.0)
                 except:
                     print 'alpha = {}, urgency = {}'.format(CongestionPercentage, Urgency)
                     sys.exit("errors!")
@@ -103,7 +107,7 @@ class StepByStepSimulator:
     def run(self):
         # create inputs
         Flows = self.geninput()
-
+        # clear outputs
         open(self.fname, 'w').close()
 
         for proto in PROTO:
@@ -112,7 +116,7 @@ class StepByStepSimulator:
                 if proto == 'dctcp' or proto == 'tcp':
                     f.bw = 2*PKTSIZE/self.RTT
 
-            Flows.sort(key=lambda f: f.startTime)
+            Flows.sort(key=lambda fs: fs.startTime)
             ReadyFlow = Flows
             ActiveFlow = []
             DeadFlow = []
@@ -122,34 +126,31 @@ class StepByStepSimulator:
                 newflows = [f for f in ReadyFlow if f.startTime == t]
                 if newflows:
                     # print 'These flows, {}, are starting at {}'.format([f.flowId for f in newflows], t)
-                    # ActiveFlow = itertools.chain(ActiveFlow, newflows)
                     ActiveFlow += newflows
-                # else:
-                    # print 'No flow is starting at {}'.format(t)
 
                 InputLoad = sum([f.bw for f in ActiveFlow])
-                print 'Input load is {}'.format(InputLoad)
+                # print 'Input load is {}'.format(InputLoad)
                 self.LINK.queue += (InputLoad * self.RTT - self.LINK.linkCap)
                 csize = 0.001
                 dropsize = 0.001
-                if (self.LINK.queue - self.ECN) < 0:
-                    self.LINK.queue = 0.001
+                if self.LINK.queue <= self.ECN:
+                    pass
+                elif self.LINK.queue <= self.MAXBUFFER:
+                    csize = self.LINK.queue - self.ECN
                 else:
-                    # LINK.queue = min(12 * MB, LINK.queue)
-                    csize = max(0.001, self.LINK.queue - self.ECN)
-                    dropsize = max(0.001, self.LINK.queue - self.MAXBUFFER)
-                    self.LINK.queue = min(self.MAXBUFFER, self.LINK.queue)
+                    csize = self.LINK.queue - self.ECN
+                    dropsize = self.LINK.queue - self.MAXBUFFER
                 print 'Queue is {}'.format(self.LINK.queue)
 
                 for f in ActiveFlow:
-                    cpercent = max(0.001, min(1, f.bw*self.RTT/csize))
+                    cpercent = max(0.000001, min(1, abs(f.bw*self.RTT/csize)))
                     if f.remainTime > 0:
                         f.bw = self.Rate(f, cpercent, self.LINK.queue)
                         f.residualRate += f.remainSize/f.remainTime - f.bw
                         f.residualRate = max(0.001, f.residualRate)
                     # print 'flow {} rate {}'.format(f.flowId, f.bw)
                     f.bw = max(0.001, f.bw)
-                    droprate = 1
+                    droprate = 0.5
                     f.remainSize = min(f.remainSize, f.remainSize - f.bw * self.RTT\
                                        + droprate * cpercent * dropsize)
                     f.remainTime -= 1
@@ -172,7 +173,7 @@ class StepByStepSimulator:
                 f.write('{}: Missed flows: {}\n'.format(proto, len(MissFlow)))
                 f.write('{}: Miss Rate: {}\n'.format(proto, 1.0*len(MissFlow)/len(Flows)))
                 f.write('\n')
-            f.close()
+                f.close()
 
 if __name__=="__main__":
     sim = StepByStepSimulator()
--- TestTransport/TUnits.py
+++ TestTransport/TUnits.py
@@ -2,6 +2,7 @@ __author__ = 'li'
 
 
 # Units
+# To reduce computation
 KB = 1.0
 MB = 1024.0 * KB
 PKTSIZE = 1.5 * KB
