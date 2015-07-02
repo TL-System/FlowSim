87fca25257a11323d62a686c0d2ce7d37a056e71
li <crischenli@gmail.com>
2015-07-02 12:01:12 +0800

    next step is to creaate a small scenario with only 10 or less flows to get a good example for mcp

--- TestTransport/GenInput.py
+++ TestTransport/GenInput.py
@@ -22,3 +22,5 @@ with open('Out/flowdump.json', 'wb') as f:
     flowdump = json.dump(flowdict, f)
 
 print json.dumps(flowdict, sort_keys=True, indent=4, separators=(',', ': '))
+
+# TODO: next step is to creaate a small scenario with only 10 or less flows to get a good example for mcp
