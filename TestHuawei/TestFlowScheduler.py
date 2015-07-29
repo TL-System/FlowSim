775067629e88da07ab430dcc50a1e4ba5d7a1d08
li <crischenli@gmail.com>
2015-07-29 13:11:23 +0800

    huawei experiments coflow output

--- TestHuawei/TestFlowScheduler.py
+++ TestHuawei/TestFlowScheduler.py
@@ -36,19 +36,39 @@ class TestFlowScheduler(FlowScheduler):
         f = open(f_name, "w")
         f_name = outDir + "plot.dat"
         f_plot = open(f_name, "w")
+        f_name = outDir + "coflow.txt"
+        f_coflow = open(f_name, "w")
+        coflow = {}
+
         for flow in self.finishedFlows:
             flowTransTime = flow.finishTime - flow.startTime
             print >> f, "%d\t%f\t%f\t%f" % (flow.flowId, flowTransTime, flow.startTime, flow.finishTime)
             flow.bw = flow.flowSize / flowTransTime
+            # processing coflows output
+            if flow.coflowId not in coflow:
+                coflow[flow.coflowId] = (flow.startTime, flow.finishTime, flowTransTime)
+            else:
+                coflowStart, coflowEnd, coflowCompletion = coflow[flow.coflowId]
+                if coflowStart > flow.startTime:
+                    coflowStart = flow.startTime
+                if coflowEnd < flow.finishTime:
+                    coflowEnd = flow.finishTime
+                coflowCompletion = coflowEnd - coflowStart
+                coflow[flow.coflowId] = (coflowStart, coflowEnd, coflowCompletion)
+
+        for k, v in coflow:
+            print >> f_coflow, "{}\t{}\t{}\t{}".format(k, v[0], v[1], v[2])
+
         # print bandwidth (in Mbps) in each line with sorted format
         bwList = [flow.bw for flow in self.finishedFlows]
         bwList.sort()
         num = len(bwList)
         for i in range(num):
             print >> f_plot, "%f\t%f" % (bwList[i] / Mb, float(i + 1) / num)
+
         f.close()
         f_plot.close()
-
+        f_coflow.close()
 
 if __name__ == "__main__":
     tfs = TestFlowScheduler()
